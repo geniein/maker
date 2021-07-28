9676416fb6d95f2f3966cc5e4278e32d87912a89
geniein <cubya0104@gmail.com>
2021-07-29 00:38:21 +0900

    Design Detail Page

--- front/pages/Detail/index.tsx
+++ front/pages/Detail/index.tsx
@@ -3,12 +3,57 @@ import Header from '@components/Header'
 import Review from '@components/Review'
 import TopMenu from '@components/TopMenu'
 import React from 'react'
+import { DetailTop } from './styles'
 
 const Detail = () => {
     return (
         <div>
             <Header/>
-            <TopMenu/>  
+            <TopMenu/> 
+            <DetailTop>
+                <form>
+                    <div className='detail_src'>
+                        <img src='/dist/images/castle.jpg' width='100%' height='auto'/>
+                    </div>
+                    <div className='detail_info'>
+                        <p className='detail_title'>First Time w Maker</p>
+                        <p className='detail_sub_price'>
+                            20,000
+                            <span className='discount_line'>50,000</span>
+                        </p>
+                        <p className='detail_tag'># Share the whole new wolrd Experience w us</p>
+                        <div>
+                            <ul className='detail_options'>
+                                <li>
+                                    <p className='detail_sub_title'>Watcher</p>
+                                    <select className='detail_sub_select'>
+                                        <option value='0'>No Options</option>
+                                        <option value='1'>Special Options</option>
+                                    </select>
+                                </li>
+                                <li>
+                                    <p className='detail_sub_title'>Netflix</p>
+                                    <select className='detail_sub_select'>
+                                        <option value='0'>Premium Netflix</option>
+                                        <option value='1'>Special Options</option>
+                                    </select>
+                                </li>
+                                <li>
+                                    <p className='detail_sub_title'>Youtube</p>
+                                    <select className='detail_sub_select'>
+                                        <option value='0'>Crazy Box</option>
+                                        <option value='1'>Special Options</option>
+                                    </select>
+                                    <p className='detail_sub_info'>[Watch out] Careful yourself</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <p className='detail_price'>20,000</p>
+                        <div className='detail_btn'>Order</div>
+                        <div className='detail_btn'>Cancel</div>
+                    </div>
+                </form>
+            </DetailTop> 
             <Review/>                      
             <Footer/>
         </div>
---
+++ front/pages/Detail/styles.tsx
@@ -0,0 +1,113 @@
+import styled from '@emotion/styled';
+
+export const DetailTop = styled.div`
+    display: inline-block;
+    width: 1200px;
+    margin-top: 70px;
+
+    .detail_src{
+        position: relative;
+        width: 644px;
+        height: 362px;
+        display: inline-flex;        
+    }
+    .detail_src_close{
+        width: 20px;
+        height: 20px;
+        position: absolute;
+        right: 10px;
+        top: 10px;
+        display: none;
+        -webkit-transition: -webkit-transform .8s ease-in-out;
+        transition: transform .8s ease-in-out;
+        cursor: pointer;
+    }
+    .detail_info{
+        width: 495px;
+        height: auto;
+        float: right;
+        text-align: left;
+    }
+    .detail_title {
+        font-size: 20px;
+        color: #383838;
+        display: inline-block;
+        margin-bottom: 10px;
+    }
+    .detail_share {
+        display: inline-block;
+        float: right;
+    }
+    .detail_sub_price{
+        font-size: 14px;
+        .discount_line{
+            text-decoration: line-through;
+            font-size: 12px;
+            margin-left: 12px;
+        }
+    }
+    .detail_tag{
+        color: #272727;
+        margin-bottom: 10px;
+    }
+    .detail_options{
+        list-style: none;
+        padding: 10px 0px 0;
+        border-top: 1px solid #ccc;
+        text-align: left;
+        border-bottom: 1px solid #ccc;
+        margin-top: 10px;
+        
+        li{
+            text-align: left;
+            margin-top: 10px;
+        }
+    }
+    .detail_sub_title{
+        display: inline-block;
+        width: 100px;
+        height: 35px;
+        line-height: 35px;
+        background: #ffffff;
+        color: #525252;
+        text-align: center;
+        border-radius: 5px;
+    }
+    .detail_sub_select{
+        border-radius: 3px;
+        width: 380px;
+        height: 40px;
+        border: 1px solid #ccc;
+        font-family: inherit;
+        background: url(/img/arrow.png) no-repeat 95% 50%;
+        background-size: 15px;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        padding-left: 10px;
+    }
+    .detail_sub_info{
+        font-size: 12px;
+        font-weight: normal;
+        margin-bottom: 0;
+        margin-top: -5px;
+        text-align: center;
+        padding: 5px 0px;
+        color: #888;
+    }
+    .detail_price{
+        text-align: center;
+        padding: 5px 0px;
+        margin-bottom: 10px;
+        border-bottom: 1px solid #ccc;
+        font-size: 13px;
+        height: 32px;
+        line-height: 32px;
+    }
+    .detail_btn{
+        cursor: pointer;
+        float: left;
+        margin: 5px 4px 5px 0px;
+        height: 45px;
+    }
+    `;
